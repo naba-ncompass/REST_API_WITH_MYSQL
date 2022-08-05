4727b183feb0b71d804b4998067b841578a719a5
naba-ncompass <naba@ncompass.in>
2022-08-05 09:50:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const userRouter = require("./api/users/user.router");
+
+app.use(express.json());
+
+app.use("/api/users", userRouter);
+const port = process.env.PORT;
+
+
+app.listen(port, () => {
+  console.log("server up and running on PORT :", port);
+});
